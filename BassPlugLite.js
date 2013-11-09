d8429620f9f79ce89d3741e5b4485f51d2e6a6fc
derpthebass <ramboxp@live.com>
2013-11-09 18:06:08 -0500

    bpl will now update if you click the bookmark while it's running

--- BassPlugLite.js
+++ BassPlugLite.js
@@ -33,31 +33,38 @@
  */
 
 bpl = {
-    autowoot: false,
+    autowoot: true,
     clicks: 0,
-    version: 3.01
+    version: 3.02,
+    close: function(){ 
+        API.off(API.DJ_ADVANCE, djAdvance); 
+        API.off(API.CHAT, chat); 
+        $('#woot').unbind('click', doubleClick);
+        }
     }
 
 function BassPlugLite(){
 window.BPLite = true;
     
 //Core Functions
-    API.on(API.DJ_ADVANCE, function(data){
-
+    djAdvance = function(data){
         if(bpl.autowoot){setTimeout(function(){
             $("#woot").click();
         }, 2000);
         }
-    });
-
-    API.on(API.CHAT, function(data){
+    };
+    
+    chat = function(data){
         if(data.message == "!whosrunning" && data.fromID == "50aeb07e96fba52c3ca04ca8"){
             API.sendChat("@"+data.from+" I am running BassPlugLite V. "+bpl.version);
         }
-    });
+    };
+    
+    API.on(API.DJ_ADVANCE, djAdvance, this);
+    API.on(API.CHAT, chat, this); 
 
 //CSS/jQuery
-    $("#woot").click(function() {
+    doubleClick = function() {
         bpl.clicks++;
         if (bpl.clicks == 2){
             bpl.autowoot = !bpl.autowoot;
@@ -67,9 +74,13 @@ window.BPLite = true;
         setTimeout(function(){
             bpl.clicks = 0;
         }, 1000);
-    });
+    };
+    
+    $("#woot").bind('click', doubleClick); 
         
 API.chatLog("Running BassPlugLite V. "+bpl.version);
 }
 
-if(typeof BPLite == "undefined" && API.enabled) BassPlugLite();
+if(typeof BPLite !== "undefined") bpl.close();
+
+BassPlugLite();
