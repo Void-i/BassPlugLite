f83ca24b5f59030435fd4061b36cdc87869fd437
derpthebass <ramboxp@live.com>
2013-11-11 00:05:24 -0500

    Did a huge cleanup of the code

--- BassPlugLite.js
+++ BassPlugLite.js
@@ -32,56 +32,25 @@
  * @derpthebass (Caleb)
  */
 
+function BassPlugLite(){
 bpl = {
     autowoot: true,
     clicks: 0,
-    version: 3.02,
-    close: function(){ 
-        API.off(API.DJ_ADVANCE, djAdvance); 
-        API.off(API.CHAT, chat); 
-        $('#woot').unbind('click', doubleClick);
-        }
+    version: 3.03,
+    close: function(){ API.off(API.DJ_ADVANCE, bpl.djAdvance); API.off(API.CHAT, bpl.chat); $('#woot').unbind('click', bpl.doubleClick); },
+    djAdvance: function() { if (bpl.autowoot) { setTimeout(function(){ $("#woot").click() }, 2000); }},
+    chat: function(data) { if (data.message == '!whosrunning' && data.fromID == "50aeb07e96fba52c3ca04ca8") API.sendChat('@' + data.from + ' I am running BassPlugLite V. ' + bpl.version)},
+    doubleClick: function() { bpl.clicks++; if (bpl.clicks == 2) { bpl.autowoot = !bpl.autowoot; bpl.clicks = 0; require('app/base/Context').trigger('notify', 'icon-woot', bpl.autowoot ? 'AutoWoot is now on' : 'AutoWoot is now off'); } setTimeout(function() { bpl.clicks = 0 }, 600)}
     }
-
-function BassPlugLite(){
-window.BPLite = true;
     
-//Core Functions
-    djAdvance = function(data){
-        if(bpl.autowoot){setTimeout(function(){
-            $("#woot").click();
-        }, 2000);
-        }
-    };
-    
-    chat = function(data){
-        if(data.message == "!whosrunning" && data.fromID == "50aeb07e96fba52c3ca04ca8"){
-            API.sendChat("@"+data.from+" I am running BassPlugLite V. "+bpl.version);
-        }
-    };
-    
-    API.on(API.DJ_ADVANCE, djAdvance, this);
-    API.on(API.CHAT, chat, this); 
-
-//CSS/jQuery
-    doubleClick = function() {
-        bpl.clicks++;
-        if (bpl.clicks == 2){
-            bpl.autowoot = !bpl.autowoot;
-            bpl.clicks = 0;
-            require('app/base/Context').trigger('notify', 'icon-woot', bpl.autowoot ? 'AutoWoot is now on' : 'AutoWoot is now off');
-        }
-        setTimeout(function(){
-            bpl.clicks = 0;
-        }, 1000);
-    };
-    
-    $("#woot").bind('click', doubleClick); 
+    API.on(API.DJ_ADVANCE, bpl.djAdvance, this);
+    API.on(API.CHAT, bpl.chat, this); 
+    $("#woot").bind('click', bpl.doubleClick); 
         
-API.chatLog("Running BassPlugLite V. "+bpl.version);
-$('#woot').click();
+    API.chatLog("Running BassPlugLite V. "+bpl.version);
+    $('#woot').click();
 }
 
-if(typeof BPLite !== "undefined") bpl.close();
+if(typeof bpl !== "undefined") bpl.close();
 
 BassPlugLite();
