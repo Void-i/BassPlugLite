a116b2c03459cf5d1e2b099a3477000d1aac8cdc
derpthebass <ramboxp@live.com>
2013-11-28 19:16:28 -0500

    Found a permanent solution for displaying the notices

--- BassPlugLite.js
+++ BassPlugLite.js
@@ -40,7 +40,7 @@ bpl = {
     close: function(){ API.off(API.DJ_ADVANCE, bpl.djAdvance); API.off(API.CHAT, bpl.chat); $('#woot').unbind('click', bpl.doubleClick); },
     djAdvance: function() { if (bpl.autowoot) { setTimeout(function(){ $("#woot").click() }, 2000); }},
     chat: function(data) { if (data.message == '!whosrunning' && data.fromID == "50aeb07e96fba52c3ca04ca8") API.sendChat('@' + data.from + ' I am running BassPlugLite V. ' + bpl.version)},
-    doubleClick: function() { bpl.clicks++; if (bpl.clicks == 2) { bpl.autowoot = !bpl.autowoot; bpl.clicks = 0; require("d2ba/aa39/fcab").trigger('notify', 'icon-woot', bpl.autowoot ? 'AutoWoot is now on' : 'AutoWoot is now off'); } setTimeout(function() { bpl.clicks = 0 }, 600)}
+    doubleClick: function() { bpl.clicks++; if (bpl.clicks == 2) { bpl.autowoot = !bpl.autowoot; bpl.clicks = 0; _$context.trigger('notify', 'icon-woot', bpl.autowoot ? 'AutoWoot is now on' : 'AutoWoot is now off'); } setTimeout(function() { bpl.clicks = 0 }, 600)}
     }
     
     API.on(API.DJ_ADVANCE, bpl.djAdvance, this);
