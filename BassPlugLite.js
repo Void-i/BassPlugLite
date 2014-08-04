d6e0ab85cf7818385ce12189cf4b0756d3670335
derpthebass <ramboxp@live.com>
2014-08-04 16:18:45 -0400

    Removed use of context

--- BassPlugLite.js
+++ BassPlugLite.js
@@ -36,11 +36,11 @@ function BassPlugLite(){
 bpl = {
     autowoot: true,
     clicks: 0,
-    version: 3.04,
+    version: 3.05,
     close: function(){ API.off(API.DJ_ADVANCE, bpl.djAdvance); API.off(API.CHAT, bpl.chat); $('#woot').unbind('click', bpl.doubleClick); },
     djAdvance: function() { if (bpl.autowoot) { setTimeout(function(){ $("#woot").click() }, 2000); }},
     chat: function(data) { if (data.message == '!whosrunning' && data.fromID == "50aeb07e96fba52c3ca04ca8") API.sendChat('@' + data.from + ' I am running BassPlugLite V. ' + bpl.version)},
-    doubleClick: function() { bpl.clicks++; if (bpl.clicks == 2) { bpl.autowoot = !bpl.autowoot; bpl.clicks = 0; _$context.trigger('notify', 'icon-woot', bpl.autowoot ? 'AutoWoot is now on' : 'AutoWoot is now off'); } setTimeout(function() { bpl.clicks = 0 }, 600)}
+    doubleClick: function() { bpl.clicks++; if (bpl.clicks == 2) { bpl.autowoot = !bpl.autowoot; bpl.clicks = 0; API.chatLog(bpl.autowoot ? 'AutoWoot is now on' : 'AutoWoot is now off'); } setTimeout(function() { bpl.clicks = 0 }, 600)}
     }
     
     API.on(API.DJ_ADVANCE, bpl.djAdvance, this);
